dbbd0b883cd8ae7df04e1c729de025f7e5c34295
KIM HYUN GI <rlagusrl928@gmail.com>
2023-08-14 21:40:12 +0900

    setup : project setup

---
+++ src/main/java/com/aswemake/api/aswemakeapitask/AswemakeApiTaskApplication.java
@@ -0,0 +1,13 @@
+package com.aswemake.api.aswemakeapitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AswemakeApiTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AswemakeApiTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aswemake/api/aswemakeapitask/AswemakeApiTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aswemake.api.aswemakeapitask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AswemakeApiTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
