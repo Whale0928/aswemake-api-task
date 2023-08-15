16eca20be2d841fd5b20339e843e0967aa3d077f
KIM HYUN GI <rlagusrl928@gmail.com>
2023-08-16 08:32:14 +0900

    chore : security Config 추가

--- src/main/java/com/aswemake/api/aswemakeapitask/common/config/security/SecurityConfig.java
+++ src/main/java/com/aswemake/api/aswemakeapitask/common/config/security/SecurityConfig.java
@@ -1,7 +1,28 @@
 package com.aswemake.api.aswemakeapitask.common.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf().disable()
+                .headers().frameOptions().disable()  // 프레임 사용 허용
+                .and()
+                .formLogin().disable()
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
 }
