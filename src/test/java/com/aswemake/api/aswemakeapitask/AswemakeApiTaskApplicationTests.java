17dc43fbde89c50e1ee7f5fad88733efe21df656
SG_COMPUTER <rlagusrl928@gmail.com>
2023-08-16 16:55:18 +0900

    feat : 사용자 로그인을 위한 샘플 데이터 일부 적재

--- src/test/java/com/aswemake/api/aswemakeapitask/AswemakeApiTaskApplicationTests.java
+++ src/test/java/com/aswemake/api/aswemakeapitask/AswemakeApiTaskApplicationTests.java
@@ -3,11 +3,8 @@ package com.aswemake.api.aswemakeapitask;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class AswemakeApiTaskApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
