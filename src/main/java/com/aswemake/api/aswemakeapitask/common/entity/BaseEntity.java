f761e08a82fcc8fb6b8eef3411f80054cc25e477
SG_COMPUTER <rlagusrl928@gmail.com>
2023-08-16 16:55:45 +0900

    feat : Entity 설정 추가

--- src/main/java/com/aswemake/api/aswemakeapitask/common/entity/BaseEntity.java
+++ src/main/java/com/aswemake/api/aswemakeapitask/common/entity/BaseEntity.java
@@ -6,12 +6,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
+@Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
