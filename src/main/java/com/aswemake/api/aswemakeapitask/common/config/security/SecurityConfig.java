08db0afd147b1b6b7e5912c6cf6d72fac738f5f9
SG_COMPUTER <rlagusrl928@gmail.com>
2023-08-16 16:56:36 +0900

    feat : 사용자 커스텀 객체 추가 , 로그인컨트롤러 생성 - 컨트롤러 소스 코드 추후 변경 예정

--- src/main/java/com/aswemake/api/aswemakeapitask/common/config/security/SecurityConfig.java
+++ src/main/java/com/aswemake/api/aswemakeapitask/common/config/security/SecurityConfig.java
@@ -1,26 +1,58 @@
 package com.aswemake.api.aswemakeapitask.common.config.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.util.Collections;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationManager customAuthenticationManager() throws Exception {
+        return new ProviderManager(Collections.singletonList(authenticationProvider()));
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());  // 비밀번호 인코더 설정
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf().disable()
+                .cors().disable()
                 .headers().frameOptions().disable()  // 프레임 사용 허용
                 .and()
                 .formLogin().disable()
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()//로그인 허용
+                                //.requestMatchers(antMatcher(GET, "/auth/market/login")).hasRole("MARKET")   //매장접근
                                 .anyRequest().authenticated()
                 )
                 .build();
---
+++ src/main/java/com/aswemake/api/aswemakeapitask/controller/AuthController.java
@@ -0,0 +1,65 @@
+package com.aswemake.api.aswemakeapitask.controller;
+
+import com.aswemake.api.aswemakeapitask.domain.user.UserRepository;
+import com.aswemake.api.aswemakeapitask.domain.user.Users;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final UserRepository userRepository; // 사용자 정보
+
+    // 일반 사용자 로그인
+    @PostMapping("/user/login")
+    public ResponseEntity<?> loginUser(/*@RequestBody LoginRequest loginRequest*/) {
+        return ResponseEntity.ok().body("User logged in successfully");
+    }
+
+    // 마켓 사용자 로그인
+    //@PostMapping("/market/login")
+    @GetMapping("/market/login")
+    public ResponseEntity<?> loginMarket(String email, HttpSession session) {
+        //서비스 동작 후 존재하는게 확실한 유저 반환
+        // 서비스 레이어 시작
+        Users users = userRepository.findByEmail(email).get();
+        // 서비스 레이어 끝
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(users.getEmail());
+
+        // 사용자 인증
+        Authentication authentication = new UsernamePasswordAuthenticationToken(users.getEmail(), users.getPassword(), userDetails.getAuthorities());
+
+        // 인증 정보 저장
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        HashMap<String, Object> userInfo = new HashMap<>();
+
+        userInfo.put("session-id",session.getId());
+        userInfo.put("id",users.getId());
+        userInfo.put("email",users.getEmail());
+        userInfo.put("name",users.getName());
+        userInfo.put("role",users.getRole());
+
+        session.setAttribute("userInfo", userInfo);
+
+        return ResponseEntity.ok().body(userInfo);
+    }
+}
