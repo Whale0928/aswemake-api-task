28f6069fe5cced73682ddafad171fc7ee9fa14f1
KIM HYUN GI <rlagusrl928@gmail.com>
2023-08-17 14:30:54 +0900

    refactor : 공통 응답 객체 개선

--- src/main/java/com/aswemake/api/aswemakeapitask/dto/GlobalResponse.java
+++ src/main/java/com/aswemake/api/aswemakeapitask/dto/GlobalResponse.java
@@ -3,6 +3,7 @@ package com.aswemake.api.aswemakeapitask.dto;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.time.LocalDateTime;
 
@@ -13,4 +14,29 @@ public class GlobalResponse {
     LocalDateTime timestamp;
     String message;
     Object data;
+
+    public static ResponseEntity<Object> ok(Object data) {
+        return buildResponse(HttpStatus.OK, "성공", data);
+    }
+
+    public static ResponseEntity<Object> ok(String message, Object data) {
+        return buildResponse(HttpStatus.OK, message, data);
+    }
+
+    public static ResponseEntity<Object> fail(Object data) {
+        return buildResponse(HttpStatus.BAD_REQUEST, "실패", data);
+    }
+
+    public static ResponseEntity<Object> fail(String message, Object data) {
+        return buildResponse(HttpStatus.BAD_REQUEST, message, data);
+    }
+
+    private static ResponseEntity<Object> buildResponse(HttpStatus status, String message, Object data) {
+        return ResponseEntity.status(status).body(GlobalResponse.builder()
+                .status(status)
+                .timestamp(LocalDateTime.now())
+                .message(message)
+                .data(data)
+                .build());
+    }
 }
