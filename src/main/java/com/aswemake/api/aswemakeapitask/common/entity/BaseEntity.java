7bb7b7f8c4e1aaf45c963d818edf5526e63ed66a
SG_COMPUTER <rlagusrl928@gmail.com>
2023-08-17 10:54:13 +0900

    feat : Item,PriceHistory 클래스 수정 개선

--- src/main/java/com/aswemake/api/aswemakeapitask/common/entity/BaseEntity.java
+++ src/main/java/com/aswemake/api/aswemakeapitask/common/entity/BaseEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -25,8 +26,10 @@ public class BaseEntity {
 
     @CreatedDate
     @Column(updatable = false)
+    @ColumnDefault("CURRENT_TIMESTAMP")
     private LocalDateTime createdDate;
 
     @LastModifiedDate
+    @ColumnDefault("CURRENT_TIMESTAMP")
     private LocalDateTime modifiedDate;
 }
---
+++ src/main/java/com/aswemake/api/aswemakeapitask/domain/item/PriceChangeStatus.java
@@ -0,0 +1,9 @@
+package com.aswemake.api.aswemakeapitask.domain.item;
+
+import lombok.Getter;
+
+@Getter
+public enum PriceChangeStatus {
+    INCREASED,  // 가격이 증가함
+    DECREASED,  // 가격이 감소함
+}
