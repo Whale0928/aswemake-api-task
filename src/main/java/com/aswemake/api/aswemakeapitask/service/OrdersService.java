a7ee6d9b61058156194fac66e666d1a01d3d522b
KIM HYUN GI <rlagusrl928@gmail.com>
2023-08-19 19:41:52 +0900

    feat : 주문번호 생성 로직 생성

--- src/main/java/com/aswemake/api/aswemakeapitask/service/OrdersService.java
+++ src/main/java/com/aswemake/api/aswemakeapitask/service/OrdersService.java
@@ -8,12 +8,14 @@ import com.aswemake.api.aswemakeapitask.domain.user.Users;
 import com.aswemake.api.aswemakeapitask.dto.orders.request.OrderCreateRequestDto;
 import com.aswemake.api.aswemakeapitask.dto.orders.response.OrderCreateResponseDto;
 import com.aswemake.api.aswemakeapitask.exception.CustomException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import static com.aswemake.api.aswemakeapitask.exception.ErrorMessages.USER_NOT_FOUND;
+import static com.aswemake.api.aswemakeapitask.service.CodeGenerator.createOrderCode;
 
 @Slf4j
 @Service
@@ -22,40 +24,22 @@ public class OrdersService {
     private final OrdersRepository ordersRepository;
     private final UserRepository userRepository;
     private final CouponCalculate couponCalculate;
+    private final ItemStockService itemStockService;
 
-    public OrderCreateResponseDto createOrder(OrderCreateRequestDto orderCreateRequestDto, Long userId) throws Exception {
-        log.info("createOrder: orderCreateRequestDto={}, userId={}", orderCreateRequestDto, userId);
+    private static Orders getBuild(OrderCreateRequestDto orderCreateRequestDto, String orderCode, Users users, Long totalAmount) {
+        return Orders.builder()
+                .orderCode(orderCode)
+                .users(users)
+                .status(OrderStatus.WAITING)
+                .zipCode(orderCreateRequestDto.getZipCode())
+                .address(orderCreateRequestDto.getAddress())
+                .packingType(orderCreateRequestDto.getPackingType())
+                .totalAmount(totalAmount)
+                .deliveryFee(orderCreateRequestDto.getDeliveryFee())
+                .build();
+    }
 
-        //주문자 조회
-        Users users = userRepository.findById(userId)
-                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, USER_NOT_FOUND));
-
-        //주문 번호 생성
-        String orderCode = createOrderCode();
-
-        //주문 아이템들 분석
-        //여기서 뭘해야할까.
-        //- 재고 감소
-
-        //총 결제 금액 계산
-        //- 쿠폰 사용
-        Long totalAmount = couponCalculate.calculateTotalPrice(orderCreateRequestDto.getOrderItems(), orderCreateRequestDto.getCouponId());
-
-        //주문 생성 및 저장
-        Orders orders = ordersRepository.saveAndFlush(
-                Orders.builder()
-                        .orderCode(orderCode)
-                        .users(users)
-                        .status(OrderStatus.WAITING)
-                        .zipCode(orderCreateRequestDto.getZipCode())
-                        .address(orderCreateRequestDto.getAddress())
-                        .packingType(orderCreateRequestDto.getPackingType())
-                        .totalAmount(totalAmount)
-                        .deliveryFee(orderCreateRequestDto.getDeliveryFee())
-                        .build()
-        );
-
-        //응답 객체 반환
+    private static OrderCreateResponseDto getOrderCreateResponseDto(Orders orders) {
         return OrderCreateResponseDto.builder()
                 .id(orders.getId())
                 .orderCode(orders.getOrderCode())
@@ -68,10 +52,31 @@ public class OrdersService {
                 .build();
     }
 
-    public String createOrderCode() {
-        return "2021090001-market-AA";
+    @Transactional
+    public OrderCreateResponseDto createOrder(OrderCreateRequestDto orderCreateRequestDto, Long userId) throws Exception {
+        log.info("createOrder: orderCreateRequestDto={}, userId={}", orderCreateRequestDto, userId);
+
+        //주문자 조회
+        Users users = userRepository.findById(userId)
+                .orElseThrow(() -> new CustomException(HttpStatus.NOT_FOUND, USER_NOT_FOUND));
+
+        //주문 번호 생성
+        String orderCode = createOrderCode(users.getId());
+
+        //재고 감소
+        itemStockService.decreaseStock(orderCreateRequestDto.getOrderItems());
+
+        //총 결제 금액 계산
+        //- 쿠폰 사용
+        Long totalAmount = couponCalculate.calculateTotalPrice(orderCreateRequestDto.getOrderItems(), orderCreateRequestDto.getCouponId());
+
+        //주문 생성 및 저장
+        Orders orders = ordersRepository.saveAndFlush(
+                getBuild(orderCreateRequestDto, orderCode, users, totalAmount)
+        );
+
+        //응답 객체 반환
+        return getOrderCreateResponseDto(orders);
     }
-
-
 }
 
